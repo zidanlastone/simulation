0ba3fddef1190a9cb4b068a10b18adc521184579
Zidan <zidanlastone01@gmail.com>
2019-09-28 00:25:53 +0700

    first commit

---
+++ assets/jsons/users.js
@@ -0,0 +1,30 @@
+let jsonUser = [
+  {
+    id: "1",
+    name: "Admin",
+    username: "MainAdmin",
+    level: "admin",
+    status: "active"
+  },
+  {
+    id: "2",
+    name: "Manager",
+    username: "MainManager",
+    level: "manager",
+    status: "active"
+  },
+  {
+    id: "3",
+    name: "Kasir 1",
+    username: "KasirPertama",
+    level: "kasir",
+    status: "active"
+  },
+  {
+    id: "4",
+    name: "Kasir 2",
+    username: "KasirKedua",
+    level: "kasir",
+    status: "not-active"
+  }
+];
